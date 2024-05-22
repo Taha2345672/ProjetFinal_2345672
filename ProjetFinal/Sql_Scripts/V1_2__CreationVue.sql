25ba5e9e11b611c6d937806e81caa57d91f5b254
tahabny <2345672@cegepmontpetit.ca>
2024-05-22 19:08:53 -0400

    FCT.Ajout creationVue

---
+++ ProjetFinal/Sql_Scripts/V1_2__CreationVue.sql
@@ -0,0 +1,8 @@
+﻿GO
+CREATE VIEW Vue_NombreAvionsParMarque AS
+SELECT Marque.NomMarque, COUNT(Avion.AvionID) AS NombreAvions
+FROM Avions.Marque
+LEFT JOIN Avions.ModeleAvion ON Marque.MarqueID = ModeleAvion.MarqueID
+LEFT JOIN Avions.Avion ON ModeleAvion.ModeleAvionID = Avion.ModeleAvionID
+GROUP BY Marque.NomMarque;
+GO
